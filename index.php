9a8b1b9a3e7b838220c46b236d5c0ce9ab0246b3
Shaxzad Nizamdinov <shaxzadnizamdinov@gmail.com>
2024-10-25 19:17:34 +0500

    Fix index.php for guessing game assignment

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
-<?php
-$correct_answer = 54;
-$user_guess = isset($_GET['guess']) ? intval($_GET['guess']) : null;
-$message = '';
-
-if ($user_guess !== null) {
-    if ($user_guess < $correct_answer) {
-        $message = 'Ваше число меньше.';
-    } elseif ($user_guess > $correct_answer) {
-        $message = 'Ваше число больше.';
-    } else {
-        $message = 'Поздравляю! Вы угадали число!';
-    }
-}
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guessing Game c4422e2c</title>
+    <title>c4422e2c</title>
 </head>
 <body>
-    <h1>Игра в угадай число</h1>
-    <form method="GET" action="">
-        <input type="number" name="guess" required>
-        <input type="submit" value="Угадать">
+    <h1>Welcome to the Guessing Game!</h1>
+    
+    <?php
+    $correct_answer = 54;
+
+    if (isset($_GET['guess'])) {
+        $guess = $_GET['guess'];
+        
+
+        if (strlen($guess) == 0) {
+            echo "<p>Your guess is too short</p>";
+        } 
+        elseif (!is_numeric($guess)) {
+            echo "<p>Your guess is not a number</p>";
+        } 
+        elseif ($guess < $correct_answer) {
+            echo "<p>Your guess is too low</p>";
+        } 
+        elseif ($guess > $correct_answer) {
+            echo "<p>Your guess is too high</p>";
+        } 
+        else {
+            echo "<p>Congratulations - You are right</p>";
+        }
+    }
+    ?>
+
+    <form method="GET">
+        <label for="guess">Enter your guess: </label>
+        <input type="text" name="guess" id="guess">
+        <input type="submit" value="Submit">
     </form>
-    <p><?php echo $message; ?></p>
 </body>
 </html>
